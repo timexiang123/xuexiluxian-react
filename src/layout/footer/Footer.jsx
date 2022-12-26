8281507c403ebe88dda422facd27ff6ca8cd4ab3
coderzwx <1308590789@qq.com>
2022-12-26 20:49:58 +0800

    feat(footer): 页面公共尾部组件开发完成

--- src/layout/footer/Footer.jsx
+++ src/layout/footer/Footer.jsx
@@ -1,12 +1,75 @@
 import React, { memo } from 'react'
-import Swiper from '@/components/swiper/swiper'
+import { Link } from 'react-router-dom'
 import { FooterWrapper } from './style'
+import logo from '@/assets/images/logo02.png'
+import factory from '@/assets/images/foot-factory.png'
+import officalWx from '@/assets/images/offical-wx.jpg'
+import teacherWx from '@/assets/images/teacher-wx.jpg'
 const Footer = memo((props) => {
   const isShow = props.isShow
   return (
     isShow && (
       <FooterWrapper>
-        <Swiper></Swiper>
+        <div className="container-xl footer-main">
+          <div className="footer-xlx">
+            <img src={logo} alt="小鹿线" title="小鹿线" />
+          </div>
+          <div className="footer-factory">
+            <img src={factory} alt="小鹿线" title="小鹿线" />
+          </div>
+          <div className="copytight">
+            <ul className="copy-top">
+              <li>
+                <Link to="/">关于我们</Link>
+              </li>
+              <li>｜</li>
+              <li>
+                <Link to="/">联系我们</Link>
+              </li>
+              <li>｜</li>
+              <li>
+                <Link to="/">意见反馈</Link>
+              </li>
+              <li>｜</li>
+              <li>
+                <Link to="/">版权声明</Link>
+              </li>
+            </ul>
+            <div className="copy-bottom">
+              <div style={{ textAlign: 'center', marginTop: '5px' }}>
+                Copyright © 2018~2038 小鹿线 www.xuexiluxian.cn
+                24小时服务热线：010-86483040
+              </div>
+              <div style={{ textAlign: 'center', marginTop: '5px' }}>
+                <Link to="/agreement/user-service" title="小鹿线用户服务协议">
+                  《小鹿线用户服务协议》
+                </Link>
+                <Link to="/agreement/privacy-policy" title="小鹿线隐私政策">
+                  《小鹿线隐私政策》
+                </Link>
+                <a
+                  href="https://beian.miit.gov.cn/#/Integrated/index"
+                  target="_blank"
+                  title="京ICP备18061704号"
+                >
+                  京ICP备18061704号
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="wx">
+            <div className="wx-bg">
+              <img src={officalWx} alt="官方微信" title="官方微信" />
+            </div>
+            <div className="wx-dsc">官方微信</div>
+          </div>
+          <div className="wx">
+            <div className="wx-bg">
+              <img src={teacherWx} alt="指导老师微信" title="指导老师微信" />
+            </div>
+            <div className="wx-dsc">指导老师微信</div>
+          </div>
+        </div>
       </FooterWrapper>
     )
   )
--- src/layout/footer/style.js
+++ src/layout/footer/style.js
@@ -1,4 +1,76 @@
 import styled from 'styled-components'
 export const FooterWrapper = styled.div`
-  color:${props=>props.theme.color.primaryColor}
+  width: 100%;
+  min-width: 1140px;
+  height: 150px;
+  background: var(--primaryColor);
+  .footer-main {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 100%;
+    color: #ffffff;
+    margin: auto;
+    img {
+      cursor: pointer;
+    }
+    .footer-xlx {
+      width: 110px;
+      opacity: 1;
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+    .footer-factory {
+      width: 130px;
+      margin: 0 20px;
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+    .copy-top {
+      font-size: 14px;
+      margin: 0 10px 10px 50px;
+      display: flex;
+      li {
+        margin: 0 10px;
+        a {
+          color: #fff;
+          &:hover {
+            text-decoration: underline;
+          }
+        }
+      }
+    }
+    .copy-bottom {
+      font-size: 12px;
+      a {
+        color: #fff;
+        margin-right: 10px;
+        &:hover {
+          text-decoration: underline;
+        }
+      }
+    }
+    .wx {
+      margin-left: 20px;
+      width: 80px;
+      height: 100px;
+      font-size: 12px;
+      .wx-bg {
+        width: 80px;
+        height: 80px;
+        img {
+          width: 100%;
+          height: 100%;
+        }
+      }
+      .wx-dsc {
+        text-align: center;
+        margin-top: 10px;
+      }
+    }
+  }
 `
